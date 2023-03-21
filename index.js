1ea0668d4568d68aa653fd1b6007016822802c03
Bryan Sosa <bry.sosa@outlook.com>
2023-03-21 18:09:16 -0400

    index js complete all test pass

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution here!
+var cats = [ 'Milo', 'Otis', 'Garfield' ];
+
+function destructivelyAppendCat(name){
+cats.push(name);
+return cats;
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+    return cats;
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+    cats;
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+    cats;
+}
+
+function appendCat(name){
+var newArray = [...cats];
+newArray.push(name);
+return newArray;
+}
+
+function prependCat(name){
+    var newArray = [...cats];
+    newArray.unshift(name);
+    return newArray;
+}
+
+function removeLastCat(){
+var newArray = [...cats];
+newArray.pop();
+return newArray;
+}
+
+function removeFirstCat(){
+    var newArray = [...cats];
+    newArray.shift();
+    return newArray; 
+}
